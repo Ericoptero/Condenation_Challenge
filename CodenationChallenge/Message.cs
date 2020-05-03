400d3514b3018ddbe142b3f5faaf88336dfcf5cd
Eric Nantes <ericnantes18@gmail.com>
2020-05-03 16:25:38 -0400

    Projeto finalizado

---
+++ CodenationChallenge/Message.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CodenationChallenge
+{
+    class Message
+    {
+        [JsonProperty("numero_casas")]
+        public int NumberOfShifts { get; set; }
+        [JsonProperty("token")]
+        public string Token { get; set; }
+        [JsonProperty("cifrado")]
+        public string Cipher { get; set; }
+        [JsonProperty("decifrado")]
+        public string Decipher { get; set; }
+        [JsonProperty("resumo_criptografico")]
+        public string Encrypted_Summary { get; set; }
+
+        public Message()
+        {
+        }
+
+        public Message(int numberOfShifts, string token, string cipher, string decipher, string encrypted_Summary)
+        {
+            NumberOfShifts = numberOfShifts;
+            Token = token;
+            Cipher = cipher;
+            Decipher = decipher;
+            Encrypted_Summary = encrypted_Summary;
+        }
+
+        public void Decoder()
+        {
+            StringBuilder decodedMessage = new StringBuilder();
+            for (int letter = 0; letter < Cipher.Length; letter++)
+            {
+                // Verifica se a iteração é uma letra, se não for, ela simplesmente armazena o char.
+                if (!char.IsLetter(Cipher[letter])) 
+                    decodedMessage.Append(Cipher[letter]);
+                else {
+                    char converter = (char)(Cipher[letter] - NumberOfShifts);
+
+                    if (converter > 'z')
+                        //Subtrai 26 caso ocorra overflow.
+                        converter = (char)(converter - 26);
+                    else if (converter < 'a')
+                        //Soma 26 caso ocorra underflow.
+                        converter = (char)(converter + 26);
+                    
+                    decodedMessage.Append(converter);
+                }
+            }
+            Decipher = decodedMessage.ToString();
+
+        }
+
+        public void GetSha1()
+        {
+            // Converte a string para um vetor de bytes e depois passamos pelo metodo de conversão SHA1.
+            var data = Encoding.ASCII.GetBytes(Decipher);
+            var hashData = new SHA1Managed().ComputeHash(data);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var b in hashData)
+            {
+                // Converte cada byte em hexadeximal.
+                sb.Append(b.ToString("x2"));
+            }
+            Encrypted_Summary = sb.ToString();
+        }
+    }
+}
---
+++ CodenationChallenge/Program.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace CodenationChallenge
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try 
+            {
+                HttpClient client = new HttpClient();
+
+                Console.Write("Write your token: ");
+                string token = Console.ReadLine();
+
+                // Realiza o GET do JSON de acordo com o token do candidato
+                string responseJson = await client.GetStringAsync("https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=" + token);
+
+                // Converte o JSON em objeto, decodifica o codigo e então o codifica para SHA1.
+                Message message = JsonConvert.DeserializeObject<Message>(responseJson);
+                message.Decoder();
+                message.GetSha1();
+
+                // Armazena o answer.json na pasta do executável.
+                var path = @$"{Path.GetFullPath("answer.json")}";
+                File.WriteAllText(path, JsonConvert.SerializeObject(message));
+
+                // POST no formato multipart/formdata
+                using (var form = new MultipartFormDataContent())
+                {
+                    // Faz uma stream do arquivo e o envia
+                    Stream fileStream = new FileStream(path, FileMode.Open);
+                    form.Add(new StreamContent(fileStream), "answer", "answer");
+
+
+                    var post = await client.PostAsync("https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=" + token, form);
+                    var result = await post.Content.ReadAsStringAsync();
+
+                    Console.WriteLine(result);
+                }
+            } 
+            catch(HttpRequestException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+
+
+        }
+    }
+}
